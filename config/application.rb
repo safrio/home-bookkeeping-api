addef352a12ff9b3568171705fe9d4564629d22d
Ivan Safonov <safrio@gmail.com>
2018-05-15 18:35:57 +0500

    Allowing CORS headers

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,12 @@ module HomeBookkeepingApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '/*', :headers => :any, :methods => :patch
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
